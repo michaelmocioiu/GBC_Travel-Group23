c1da1065e2a0ed42bfdf553e0bee2333838e314d
michaelmocioiu <michaelmocioiu@gmail.com>
2024-02-23 17:41:31 -0500

    aa

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,6 @@ namespace GBC_Travel_Group23.Controllers
     {
         public IActionResult Index()
         {
-            System.Console.WriteLine("aa");
             return View();
         }
     }
