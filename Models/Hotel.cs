1f496c41e3863713900660082257b7a6fb832e67
michaelmocioiu <michaelmocioiu@gmail.com>
2024-02-23 20:31:26 -0500

    .

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -1,38 +1,28 @@
 ﻿using Mono.TextTemplating;
-using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
 namespace GBC_Travel_Group23.Models
 {
     public class Hotel : Service
     {
         public int Id { get; set; }
-        [Required] public string Name { get; set; }
         [Required] public string Country { get; set; }
-        [Required] public string City { get; set; }
-        [Required] public string Address { get; set; }
-        [Required] public string[] Amenities { get; set; }
-        [Required] public string RoomInfoJson { get; set; }
-        public List<HotelRoom> Rooms
-        {
-            get { return JsonConvert.DeserializeObject<List<HotelRoom>>(RoomInfoJson)!; }
-        }
 
-        public string SerializeRoomInfo()
-        {
-            return JsonConvert.SerializeObject(Rooms);
-        }
-        public HotelRoom? GetPriciestAvailableRoom()
+        [Required] public string City { get; set; }
+
+        [Required] public string Address { get; set; }
+
+        [Required] public string[] Amenities { get; set; }
+        [Required] public List<HotelRoom> Rooms { get; set; }
+        public HotelRoom? GetPriciestRoom()
         {
             return Rooms
-                .Where(room => room.IsAvailable)
                 .OrderByDescending(seat => seat.NightlyRate)
                 .FirstOrDefault();
         }
 
-        public HotelRoom? GetCheapestAvailableRoom()
+        public HotelRoom? GetCheapestRoom()
         {
             return Rooms
-                .Where(room => room.IsAvailable)
                 .OrderBy(seat => seat.NightlyRate)
                 .FirstOrDefault();
         }
@@ -40,17 +30,10 @@ namespace GBC_Travel_Group23.Models
 
     public class HotelRoom
     {
-        public int RoomNumber { get; set; }
-        [Required]
-        public bool IsAvailable { get; set; }
-
-        [Required]
-        public int BedCount { get; set; }
-        [Required]
-        public int BathCount { get; set; }
-        [Required]
-        public string[] Amenities { get; set; }
-        [Required]
-        public double NightlyRate { get; set; }
+        [Required] public int RoomNumber { get; set; }
+        [Required] public int BedCount { get; set; }
+        [Required] public int BathCount { get; set; }
+        [Required] public string[] Amenities { get; set; }
+        [Required] public double NightlyRate { get; set; }
     }
 }
